00799b6a20b4f40e8800eb886cadd7d225beeb45
tryfan <nick@celebic.net>
2020-03-06 10:09:11 -0500

    updating ISO info

--- centos.sh
+++ centos.sh
@@ -1,1 +1,7 @@
-./morpheus-library -base=vms/linux -template vms/linux/templates/centos/centos-7/centos-base.json -var-file vms/linux/templates/centos/centos-7/centos-7_5-x86_64.json -sudo -var "git_hash=$GIT_HASH" -only=kvm
+./morpheus-library \
+-base=vms/linux \
+-template vms/linux/templates/centos/centos-7/centos-base.json \
+-var-file vms/linux/templates/centos/centos-7/centos-7_5-x86_64.json \
+-sudo \
+-var "git_hash=$GIT_HASH" \
+-only=kvm
